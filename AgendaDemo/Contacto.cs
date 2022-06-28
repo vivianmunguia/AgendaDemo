c2a7635a3f125164d6d53ac6f9164bb448e389ec
Vivian Munguía <andy_munguia@hotmail.com>
2022-06-27 20:41:09 -0700

    The project was initialized and the contact class was created

---
+++ AgendaDemo/Contacto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgendaDemo
+{
+    public class Contacto
+    {
+        public string Nombre { get; set; }
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
+    }
+}
