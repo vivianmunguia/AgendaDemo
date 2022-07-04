88930bbdf4cef9fc1a9ef5ebf7d872afece1c1dc
Vivian Munguía <andy_munguia@hotmail.com>
2022-07-03 21:02:22 -0700

    The list was filled with a collection of contacts and the contactpage was filled with the information of the selected contact

---
+++ AgendaDemo/PaginaListaContactos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AgendaDemo
+{
+    public class PaginaListaContactos : MasterDetailPage
+    {
+        public PaginaListaContactos()
+        {
+            var lista = new ListView();
+            lista.ItemsSource = GeneradorDeContactos.CrearContactos().OrderBy(x => x.Nombre);
+
+            Master = new ContentPage
+            {
+                Title = "Contactos",
+                Content = lista
+            };
+
+            Detail = new PaginaContacto(GeneradorDeContactos.CrearContactos().First());
+        }
+    }
+}
