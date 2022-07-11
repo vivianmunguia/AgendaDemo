eb3e81ed2cc4517ef84577772ff965da5029dc4c
Vivian Munguía <andy_munguia@hotmail.com>
2022-07-10 18:31:28 -0700

    The information in the main page changes when a different contact is selected

--- AgendaDemo/Contacto.cs
+++ AgendaDemo/Contacto.cs
@@ -9,5 +9,10 @@ namespace AgendaDemo
         public string Nombre { get; set; }
         public string Telefono { get; set; }
         public string Direccion { get; set; }
+
+        public override string ToString()
+        {
+            return Nombre;
+        }
     }
 }
--- AgendaDemo/PaginaListaContactos.cs
+++ AgendaDemo/PaginaListaContactos.cs
@@ -12,7 +12,16 @@ namespace AgendaDemo
         {
             var lista = new ListView();
             lista.ItemsSource = GeneradorDeContactos.CrearContactos().OrderBy(x => x.Nombre);
-            
+
+            lista.ItemSelected += (sender, e) =>
+            {
+                if (e.SelectedItem != null)
+                {
+                    Detail = new PaginaContacto(e.SelectedItem as Contacto);
+                    IsPresented = false;
+                }
+            };
+
             Master = new ContentPage
             {
                 Title = "Contactos",
